768832420c65db8c59f4d6699497a8eaab2c16e0
Jacek Chleborowicz <j.chleborowicz@gmail.com>
2020-07-31 20:54:48 +0200

    Initial commit.

---
+++ src/main/java/pl/jch/tests/spring/springdocstest/SpringDocsTestApplication.java
@@ -0,0 +1,13 @@
+package pl.jch.tests.spring.springdocstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDocsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDocsTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/jch/tests/spring/springdocstest/SpringDocsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jch.tests.spring.springdocstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDocsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
